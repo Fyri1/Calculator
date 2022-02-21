8e501d74c6b749dc1c9ce6cfb0ed7adc77942af2
VILSAN <49275632+Fyri1@users.noreply.github.com>
2022-02-21 15:21:17 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Поле в котором все выводится
+let input = document.querySelector('.input');
+//Сохраненная часть выражения для возведения в степень
+let power = "";
+//Поле в котором все выводится
+
+
+function insert(num)
+{
+    if( input.textContent == 0 )
+    {
+
+    }
+    
+}
